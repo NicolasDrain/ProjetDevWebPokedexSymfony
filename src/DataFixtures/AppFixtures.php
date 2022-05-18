06482393787baddd0e9c7ba315e578aaea0aae08
Nicolas Drain <nicodrain@hotmail.fr>
2022-05-19 01:08:49 +0200

    Ajout de DataFixtures + ajout jeu de données type

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Type;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        $array_type = [
+            'acier',
+            'combat',
+            'dragon',
+            'eau',
+            'electrik',
+            'feu',
+            'glace',
+            'insecte',
+            'normal',
+            'plante',
+            'poison',
+            'psy',
+            'roche',
+            'sol',
+            'spectre',
+            'vol',
+            'fée'
+        ];
+        foreach($array_type as $type){
+            $type = new Type();
+            $manager->persist($type);
+        }
+        $manager->flush();
+    }
+}
