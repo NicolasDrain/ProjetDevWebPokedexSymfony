7188bb80eeb87b9bf169af64458b20dedb4c4fdf
Nicolas Drain <nicodrain@hotmail.fr>
2022-05-19 12:05:41 +0200

    Mise à jours fixtures

---
+++ src/DataFixtures/TypeFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Type;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TypeFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
