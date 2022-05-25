f19552d8bf3457889800cc822f1f5bb533804f00
adtoxic2 <86926308+adtoxic2@users.noreply.github.com>
2022-05-25 23:32:35 +0200

    ajout systeme connexion et inscription

--- src/Entity/Dresseur.php
+++ src/Entity/Dresseur.php
@@ -6,10 +6,12 @@ use App\Repository\DresseurRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: DresseurRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Dresseur implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -41,6 +43,8 @@ class Dresseur implements UserInterface, PasswordAuthenticatedUserInterface
     public function __construct()
     {
         $this->pokemonDresseurs = new ArrayCollection();
+        $this->starter_taken = false;
+        $this->argent = 5000;
     }
 
     public function getId(): ?int
