e39aca88171f167fad44175bb683ae55171b6984
Nicolas Drain <nicodrain@hotmail.fr>
2022-05-25 21:43:27 +0200

    Ajout fixtures pokemon et type

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Type;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,31 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $array_type = [
+            'acier',
+            'combat',
+            'dragon',
+            'eau',
+            'electrik',
+            'feu',
+            'glace',
+            'insecte',
+            'normal',
+            'plante',
+            'poison',
+            'psy',
+            'roche',
+            'sol',
+            'spectre',
+            'vol',
+            'fée'
+        ];
+        foreach($array_type as $type){
+            $new_type = new Type();
+            $new_type->setNom($type);
+            $this->addReference($new_type->getNom(),$new_type);
+            $manager->persist($new_type);
+        }
         $manager->flush();
     }
 }
