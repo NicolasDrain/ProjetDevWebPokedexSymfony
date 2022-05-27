66f01185c4d5e25f257bbaded99937d7677a9e2c
Nicolas Drain <nicodrain@hotmail.fr>
2022-05-27 18:02:09 +0200

    Visualisation des pokémons de l'utilisateur en cours, à terminer

---
+++ src/Controller/MesPokemonsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PokemonDresseurRepository;
+use App\Entity\PokemonDresseur;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MesPokemonsController extends AbstractController
+{
+    #[Route('/mesPokemons', name: 'app_mes_pokemons')]
+    #[IsGranted('ROLE_USER')]
+    public function index(PokemonDresseurRepository $pokemonDresseurRepository): Response
+    {
+        $dresseur = $this->getUser();
+        $pokemonDresseur = $pokemonDresseurRepository->findBy(array('id_dresseur' => $dresseur));
+        return $this->render('mes_pokemons/index.html.twig', [
+            'pokemonDresseur' => $pokemonDresseur,
+        ]);
+    }
+
+    #[Route('/mesPokemons/{id}', name: 'app_modify_pokemon')]
+    #[Security("is_granted('ROLE_USER') and user === pokemonDresseur.getIdDresseur()")]
+    public function modifyPokemon(PokemonDresseur $pokemonDresseur): Response
+    {
+        dd($pokemonDresseur);
+        $dresseur = $this->getUser();
+        return $this->render('mes_pokemons/index.html.twig', [
+            'pokemonDresseur' => $pokemonDresseur,
+        ]);
+    }
+}
